d14f1b31fc1c1d1e9fb55820100b98fcdd9d8c1b
Acunetix 360 <50905181+acunetix360dev@users.noreply.github.com>
2020-01-10 12:15:00 +0300

    Update publish-alpha.sh

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -2,5 +2,5 @@
 
 set -e
 circleci config pack src > orb.yml
-circleci orb publish orb.yml acunetix-360/acunetix-360@1.1.0
+circleci orb publish orb.yml acunetix-360/acunetix-360@1.1.2
 rm -rf orb.yml
